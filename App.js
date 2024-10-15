b597e7c6451fa0bb1f682e3e3752e09965503037
Hanna-Mari Kielenniva <h.kielenniva@gmail.com>
2024-10-15 14:18:40 +0300

    Valmis

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Button, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+
+  const speak = () => {
+    const thingToSay = text; //tämä määrittelee teksin, joka puhutaan. 
+    Speech.speak(thingToSay);//tämä kutsuu Expo Speech -moduulin speak-metodia, joka muuttaa thingToSay-muuttujan sisällön puheeksi.
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+       <TextInput 
+        placeholder='Write text' 
+        onChangeText={text => setText(text)}
+        value={text} 
+        style={styles.input}
+      /> 
+      <Button title="Press to hear text" onPress={speak} />
     </View>
   );
 }
@@ -13,8 +27,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  input: {
+    height: 50, 
+    borderColor: 'black', 
+    borderWidth: 1, 
+    marginBottom: 20, 
+    paddingHorizontal: 10, 
+    width: '70%', 
   },
 });
